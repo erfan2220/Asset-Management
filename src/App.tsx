76aef14345182a5bf3cce031ab5eebe949273889
Erfan Hashemi <e.hashemi2220@gmai.com>
2024-08-06 13:37:02 +0330

    Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import React from "react";
-import {BrowserRouter, Routes, Route, Outlet} from 'react-router-dom';
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import Navbar from "./Components/Navbar/Navbar";
+import Asset from "./Pages/AssetPage/index";
+import Dashboard from "./Pages/Dashboard/Dashboard"; // Directly import Dashboard
 
-
-
-const Dashboard = React.lazy(()=>import('./Pages/Dashboard/Dashboard'))
-
-
-const Layout =()=>{
-    return(<>
-            <Navbar/>
-               <Outlet/>
-        </>)
-}
-
-
-
+const Layout = () => (
+    <>
+        <Navbar />
+        <div className="flex">
+            <Dashboard />
+            <div className="flex flex-row">
+                <Outlet />
+            </div>
+        </div>
+    </>
+);
 
 function App() {
-  return (
-   <BrowserRouter>
-       <Routes>
-           <Route path="/" element={<Layout />}>
-               <Route path="/" element={<Dashboard/>} />
-               <Route path="/dashboard" element={<Dashboard/>} />
-           </Route>
-       </Routes>
-   </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Asset />} />
+                    <Route path="/dashboard" element={<div>Home</div>} />
+                    <Route path="/asset" element={<Asset />} />
+
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,148 +1,147 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ReactComponent as AssetIcon } from "./Menu/6.svg";
+import { ReactComponent as LocationIcon } from "./Menu/4.svg";
+import { ReactComponent as WorkOrderIcon } from "./Menu/3.svg";
+import { ReactComponent as ScoreIcon } from "./Menu/2.svg";
+import { ReactComponent as AdminIcon } from "./Menu/1.svg";
+import Table from "../../Components/AssetTable/Table"
+import Pagination from "../../Components/AssetTable/pagination/Pagination";
 
-import React, {useEffect, useState} from 'react';
-import {ReactComponent as AssetIcon} from "./Menu/6.svg";
-import {ReactComponent as LocationIcon} from "./Menu/4.svg";
-import {ReactComponent as WorkOrderIcon} from "./Menu/3.svg";
-import {ReactComponent as ScoreIcon} from "./Menu/2.svg";
-import {ReactComponent as AdminIcon} from "./Menu/1.svg";
-
-
-interface MenuItem {
-    name: string;
-    subMenu: string[];
-}
-
-
-const menu:MenuItem[]=[
+const menu = [
     {
-        name:"Asset",
-        subMenu:[
-            "Data table ",
-            "Map",
-            "Charts",
-            "Matrix"
-        ]
+        name: "Asset",
+        subMenu: ["Data table ", "Map", "Charts", "Matrix"],
+        route: "/asset",
     },
     {
-        name:"Location",
-        subMenu:[]
+        name: "Location",
+        subMenu: [],
+        route: "/location",
     },
     {
-        name:"Work order queues",
-        subMenu:[]
-    },    {
-        name:"Score Settings",
-        subMenu:[]
+        name: "Work order queues",
+        subMenu: [],
+        route: "/work-order-queues",
     },
     {
-        name:"Admin Panel",
-        subMenu:[]
-    },]
+        name: "Score Settings",
+        subMenu: [],
+        route: "/score-settings",
+    },
+    {
+        name: "Admin Panel",
+        subMenu: [],
+        route: "/admin-panel",
+    },
+];
 
+const Dashboard = () => {
+    const [currentIndex, setCurrentIndex] = useState(1);
+    const [openText, setOpenText] = useState(false);
+    const [menuName, setMenuName] = useState("Asset");
+    const navigate = useNavigate();
 
+    const handleIndex = (index:number, name:string, route:string) => {
+        setCurrentIndex(index);
+        setMenuName(name);
+        navigate(route);
+    };
 
-const Dashboard = () =>
-{
-    const [currentIndex,setCurrentIndex] = useState(1);
-    const [openText,setOpenText]=useState(false)
-    const [menuName,setMenuName]=useState("")
+    const handleMenuOpen = (condition:any) => {
+        setOpenText(condition);
+    };
 
+    useEffect(() => {
+        let timeout:any;
 
-    const handleIndex =(index:number,name:string)=>{
-        setCurrentIndex(index)
-        setMenuName(name)
-    }
-    const handleMenuOpen =(condition:boolean)=>{
-        setOpenText(condition)
-    }
-
-    useEffect(()=>{
-        let timeout:ReturnType<typeof setTimeout>;
-
-
-        if(openText){
-            timeout=setTimeout(()=>setOpenText(true),300);
-        }
-        else
-        {
+        if (openText) {
+            timeout = setTimeout(() => setOpenText(true), 300);
+        } else {
             timeout = setTimeout(() => setOpenText(false), 300);
         }
 
-
-
-
-    })
-
-
-
-
-
-
-
-
-
+        return () => clearTimeout(timeout);
+    }, [openText]);
 
     return (
-        <div>
-            {/*Menu Section*/}
-            <div className="flex flex-col gap-[20px] bg-[#14141A] pt-[22px] pr-[28px] max-w-fit
-             min-h-[calc(100vh-64px)] pl-[32px] cursor-pointer"
-                 onMouseEnter={()=>{handleMenuOpen(true)}} onMouseLeave={()=>handleMenuOpen(false)}>
-                {
-                    menu.map((item,index)=>(
-                        <div className="flex flex-row items-center gap-[12px]" onClick={()=>handleIndex(index,item.name)}>
-                            {item.name === 'Asset' && (
-                                <AssetIcon
-                                    className={`${currentIndex === index ? 'text-[#007BFF]' : 'text-[#FFF]'}`}
-                                    stroke={currentIndex === index ? '#007bff' : '#ffffff'}
-                                />
-                            )}
-                            {
-                                (item.name === "Location" ) &&
-                                   <LocationIcon
-                                       className={` ${currentIndex === index ? 'text-[#007BFF]' : 'text-[#FFF]'}`}
-                                       stroke={currentIndex === index ? '#007bff' : '#ffffff'}
-                                   />
-                            }
-                            {
-                                (item.name === "Work order queues" ) &&
-                                   <WorkOrderIcon
-                                       className={` ${currentIndex === index ? 'text-[#007BFF]' : 'text-[#FFF]'}`}
-                                       stroke={currentIndex === index ? '#007bff' : '#ffffff'}
-                                   />
-                            }
-                            {
-                                (item.name === "Score Settings" ) &&
-                                   <ScoreIcon
-                                       className={` ${currentIndex === index ? 'text-[#007BFF]' : 'text-[#FFF]'}`}
-                                       stroke={currentIndex === index ? '#007bff' : '#ffffff'}
-                                   />
-                            }
-                            {
-                                (item.name === "Admin Panel" ) &&
-                                   <AdminIcon
-                                       className={` ${currentIndex === index ? 'text-[#007BFF]' : 'text-[#FFF]'}`}
-                                       stroke={currentIndex === index ? '#007bff' : '#ffffff'}
-                                   />
-                            }
+        <div className="flex flex-col items-center">
+            <div
+                className="flex flex-col gap-[20px] bg-[#14141A] pt-[22px] pr-[28px] max-w-fit
+               min-h-[calc(100vh-64px)] pl-[32px] cursor-pointer"
+                onMouseEnter={() => {
+                    handleMenuOpen(true);
+                }}
+                onMouseLeave={() => handleMenuOpen(false)}
+            >
+                {menu.map((item, index) => (
+                    <div
+                        key={item.name}
+                        className="flex flex-row items-center gap-[12px]"
+                        onClick={() => handleIndex(index, item.name, item.route)}
+                    >
+                        {item.name === "Asset" && (
+                            <AssetIcon
+                                className={`${
+                                    currentIndex === index ? "text-[#007BFF]" : "text-[#FFF]"
+                                }`}
+                                stroke={currentIndex === index ? "#007bff" : "#ffffff"}
+                            />
+                        )}
+                        {item.name === "Location" && (
+                            <LocationIcon
+                                className={`${
+                                    currentIndex === index ? "text-[#007BFF]" : "text-[#FFF]"
+                                }`}
+                                stroke={currentIndex === index ? "#007bff" : "#ffffff"}
+                            />
+                        )}
+                        {item.name === "Work order queues" && (
+                            <WorkOrderIcon
+                                className={`${
+                                    currentIndex === index ? "text-[#007BFF]" : "text-[#FFF]"
+                                }`}
+                                stroke={currentIndex === index ? "#007bff" : "#ffffff"}
+                            />
+                        )}
+                        {item.name === "Score Settings" && (
+                            <ScoreIcon
+                                className={`${
+                                    currentIndex === index ? "text-[#007BFF]" : "text-[#FFF]"
+                                }`}
+                                stroke={currentIndex === index ? "#007bff" : "#ffffff"}
+                            />
+                        )}
+                        {item.name === "Admin Panel" && (
+                            <AdminIcon
+                                className={`${
+                                    currentIndex === index ? "text-[#007BFF]" : "text-[#FFF]"
+                                }`}
+                                stroke={currentIndex === index ? "#007bff" : "#ffffff"}
+                            />
+                        )}
 
-                            {
-                                openText && (
-                                    <h2 className={currentIndex === index ? "text-[16px] text-[#007BFF]" : "text-[16px] text-[#FFF] opacity-60"}>{item.name}</h2>)
-                            }
+                        {openText && (
+                            <h2
+                                className={
+                                    currentIndex === index
+                                        ? "text-[16px] text-[#007BFF]"
+                                        : "text-[16px] text-[#FFF] opacity-60"
+                                }
+                            >
+                                {item.name}
+                            </h2>
+                        )}
+                    </div>
+                ))}
 
-                        </div>
-                    ))
-                }
+
             </div>
-            <div>
-                <div>
 
-                </div>
 
-            </div>
 
         </div>
+
     );
 };
 
